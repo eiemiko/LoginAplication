80b4c874bdb2a87ee64ddbe4a154e4f09c124f22
Emily <emily.lemos@etec.sp.gov.br>
2019-04-02 22:26:18 -0300

    Add user password and button

--- app/src/main/java/br/com/etecia/loginaplication/MainActivity.java
+++ app/src/main/java/br/com/etecia/loginaplication/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.etecia.loginaplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user = agfhj
+        Button btn_log = findViewById(R.id.btn_log);
+        btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView lbl_user = findViewById(R.id.lbl_user);
+                TextView lbl_pass = findViewById(R.id.lbl_pass);
+                String username = lbl_user.getText().toString();
+                String password = lbl_pass.getText().toString();
+
+            }
+        });
     }
 }
